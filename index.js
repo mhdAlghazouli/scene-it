6e8f37d1b61783b0c5a10ebbe8af4818a6e3af44
mhd1995 <35552987+mhd1995@users.noreply.github.com>
2022-08-30 21:19:56 -0400

    second commit in index.js

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
+const moviesContainer = document.getElementById("moviesContainer");
+const myForm = document.getElementById('search-form');
+
+document.addEventListener('DOMContentLoaded', 
+function() {
   // code here will execute after the document is loaded
+  
+  myForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    moviesContainer.innerHTML = renderMovies(movieData);
+    // event listener code goes here
+    });
+  
 });
 
 function renderMovies(movieArray) {
@@ -8,5 +19,6 @@ function renderMovies(movieArray) {
     <h2>${currentMovie.Title}</h2></div>`
   });
   
-  movieHtmlArray.join('')
+  return movieHtmlArray.join('')
+  
 }
