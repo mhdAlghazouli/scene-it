bba2385f1a4d8758570c59dd04e7505341ad5c06
mhd1995 <35552987+mhd1995@users.noreply.github.com>
2022-09-01 00:02:27 -0400

    last commit in watchlist.js

--- watchlist.js
+++ watchlist.js
@@ -13,11 +13,16 @@ document.addEventListener('DOMContentLoaded', (e) => {
 function renderMovies(movieArray) {
   let movieHtmlArray = movieArray.map((currentMovie) => {
     return `
-    <img src="${currentMovie.Poster}" class="card-img-bottom"/>
-    <div class='card-body'>
-    <h5 class='card-title'>${currentMovie.Title}</h5>
-    <p style="background-color:gray;">${currentMovie.Year}</p></div>
-    <button class="btn btn-primary add-button" data-imdbid="${currentMovie.imdbID}">Add!</button>`
+    <div style="background-color:white; width:18rem; margin-bottom:16px" >
+    <img src="${currentMovie.Poster}" style="width:18rem; height:400px"/>
+    <div class='movie card-body d-flex flex-row justify-content-between  align-items-center'>
+    <h5 class='card-title'>${currentMovie.Title}
+    <span style="font-size:15px" class="card-text text-light bg-dark">${currentMovie.Year}</span>
+    </h5>
+    </div>
+    <button class="btn btn-primary btn-lg add-button  ml-3 mb-3" data-imdbid="${currentMovie.imdbID}"  style="width:90px;">Add!</button>
+    </div>
+    `
   });
   
   return movieHtmlArray.join('')
